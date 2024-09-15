5e6e736601a46efa2f1d40c620f86b1ba4cf387e
DebugJedi <89666546+DebugJedi@users.noreply.github.com>
2024-09-15 19:06:52 -0400

    Added stream.py

---
+++ stream.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+# Page Setup
+
+about_page = st.Page(
+    page="view/about_me.py",
+    title="About Me",
+    icon = ":material/account_circle:",
+    default=True,
+)
+project_1_page = st.Page(
+    page="view/dashboard.py",
+    title="Dashboard",
+    icon = ":material/bar_chart:"
+)
+project_2_page = st.Page(
+    page= "view/chatbot.py",
+    title="Chat Bot",
+    icon=":material/smart_toy:",
+)
+
+# Navigation setup [WITHOUT SECTIONS]
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# Navigation setup [WITH SECTION]
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page]
+
+    }
+)
+
+# Shared On all Pages
+
+st.logo("Assets/Logo.png")
+st.sidebar.text("By DebugJedi")
+
+# Run Navigation
+pg.run()
