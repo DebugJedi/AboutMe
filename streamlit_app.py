e1c92406d0eb68c61ee67c0506cf69da7c39d08c
priyank rao <priyankrao2512@gmail.com>
2024-09-22 02:12:13 -0400

    pages removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,16 @@ about_page = st.Page(
     icon = ":material/account_circle:",
     default=True,
 )
-project_1_page = st.Page(
-    page="view/dashboard.py",
-    title="Dashboard",
-    icon = ":material/bar_chart:"
-)
-project_2_page = st.Page(
-    page= "view/chatbot.py",
-    title="Chat Bot",
-    icon=":material/smart_toy:",
-)
+# project_1_page = st.Page(
+#     page="view/dashboard.py",
+#     title="Dashboard",
+#     icon = ":material/bar_chart:"
+# )
+# project_2_page = st.Page(
+#     page= "view/chatbot.py",
+#     title="Chat Bot",
+#     icon=":material/smart_toy:",
+# )
 
 # Navigation setup [WITHOUT SECTIONS]
 # pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
