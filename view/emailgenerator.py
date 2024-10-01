751dcb803edb0855e22599f9d254b6e1a42574b9
DebugJedi <priyankrao2512@gmail.com>
2024-10-01 02:00:41 -0400

    added intro

--- view/emailgenerator.py
+++ view/emailgenerator.py
@@ -3,19 +3,16 @@ import streamlit as st
 # with open("assets/style.css") as f:
 #         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True) 
 
-com1, com2, com3 = st.columns([3,0.5,0.5], gap= 'small', vertical_alignment='center')
+com1, com2 = st.columns([3,1], gap= 'small', vertical_alignment='center')
 
 url = 'https://coldemail-5zdtzmmqe7nns6pgcekxuw.streamlit.app/'
 with com1:
     
-    st.markdown("# [Email Hiring Manager](%s)"% url)  
+    st.markdown("# [Email Hiring Manager]🔗(%s)"% url)  
     
     
 with com2:
-    st.write("🔗")
-    # st.markdown("[link](%s):material/link:"% url)   
-with com3:
-    st.image("Assets/old_computer.jpg")
+   st.image("Assets/old_computer.jpg")
 
 
 introduction = """
